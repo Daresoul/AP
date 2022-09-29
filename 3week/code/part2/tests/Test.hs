d1864e1512b10c59695fbf552ccb5a4bb77b6b25
Mikkel <right-mik@hotmail.com>
2022-09-29 18:32:04 +0200

    tests

--- 3week/code/part2/tests/Test.hs
+++ 3week/code/part2/tests/Test.hs
@@ -9,15 +9,21 @@ import Test.Tasty.HUnit
 main :: IO ()
 main = defaultMain $ localOption (mkTimeout 1000000) tests
 
-tests = testGroup "Minimal tests" [
-  testCase "simple success" $
-    parseString "'\\'" @?=
-      Right [SExp (Const (StringVal "\\"))],
-  testCase "simple success" $
-    parseString "2 + two" @?=
-      Right [SExp (Oper Plus (Const (IntVal 2)) (Var "two"))],
-  testCase "simple failure" $
-    -- avoid "expecting" very specific parse-error messages
-    case parseString "wow!" of
-      Left e -> return ()  -- any message is OK
-      Right p -> assertFailure $ "Unexpected parse: " ++ show p]
+tests :: TestTree
+tests = testGroup "AllTests" [testingMathOperators]
+
+testingMathOperators :: TestTree
+testingMathOperators = testGroup "testingMathOperators"
+  [testCase "Plus 2+2" $ parseString "2+2" @?= Right [SExp (Oper Plus (Const (IntVal 2)) (Const (IntVal 2)))],
+   testCase "Plus (-2)+2" $ parseString "(-2)+2" @?= Right [SExp (Oper Plus (Const (IntVal (-2) )) (Const (IntVal 2)))],
+   testCase "Plus -2+2" $ parseString "-2+2" @?= Right [SExp (Oper Plus (Const (IntVal (-2) )) (Const (IntVal 2)))],
+   testCase "Plus (-2)(-2)" $ parseString "(-2)+(-2)" @?= Right [SExp (Oper Plus (Const (IntVal (-2) )) (Const (IntVal (-2))))],
+   testCase "Plus left asso 2+2+2" $ parseString "2+2+2" @?= Right [SExp (Oper Plus (Oper Plus (Const (IntVal 2)) (Const (IntVal 2))) (Const (IntVal 2)))],
+   testCase "Minus 2-2" $ parseString "2-2" @?= Right [SExp (Oper Minus (Const (IntVal 2)) (Const (IntVal 2)))],
+   testCase "Minus -2-2" $ parseString "-2-2" @?= Right [SExp (Oper Minus (Const (IntVal (-2))) (Const (IntVal 2)))],
+   testCase "Minus (-2)(-2)" $ parseString "(-2)-(-2)" @?= Right [SExp (Oper Minus (Const (IntVal (-2))) (Const (IntVal (-2))))],
+   testCase "Minus left asso 2-2-2" $ parseString "2-2-2" @?= Right [SExp (Oper Minus (Oper Minus (Const (IntVal 2)) (Const (IntVal 2))) (Const (IntVal 2)))],  
+   ]
+
+-- testingFuncEval :: TestTree
+-- testingFuncEval = testGroup "TestingFuncEval"
