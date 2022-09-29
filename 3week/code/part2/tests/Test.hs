c635f82523586b32b4d36af5dd0ca9d0710206f8
Mikkel <right-mik@hotmail.com>
2022-09-29 19:59:30 +0200

    more tests

--- 3week/code/part2/tests/Test.hs
+++ 3week/code/part2/tests/Test.hs
@@ -24,9 +24,13 @@ testingMathOperators = testGroup "testingMathOperators"
    testCase "Minus (-2)(-2)" $ parseString "(-2)-(-2)" @?= Right [SExp (Oper Minus (Const (IntVal (-2))) (Const (IntVal (-2))))],
    testCase "Minus left asso 2-2-2" $ parseString "2-2-2" @?= Right [SExp (Oper Minus (Oper Minus (Const (IntVal 2)) (Const (IntVal 2))) (Const (IntVal 2)))],  
    testCase "Minus Plus left asso 2-2+2" $ parseString "2-2+2" @?= Right [SExp (Oper Plus (Oper Minus (Const (IntVal 2)) (Const (IntVal 2))) (Const (IntVal 2)))],  
-   testCase "Plus Minus left asso 2+2-2" $ parseString "2+2-2" @?= Right [SExp (Oper Minus (Oper Plus (Const (IntVal 2)) (Const (IntVal 2))) (Const (IntVal 2)))],  
-   
-   ]
-
+   testCase "Plus Minus left asso 2+2-2" $ parseString "2+2-2" @?= Right [SExp (Oper Minus (Oper Plus (Const (IntVal 2)) (Const (IntVal 2))) (Const (IntVal 2)))],
+   testCase "char string test 'lolqopbob' " $ parseString "'lolqopbob'" @?= Right [SExp (Const (StringVal "lolqopbob"))],
+   testCase "int string test 'lolqopbob' " $ parseString "'123561636'" @?= Right [SExp (Const (StringVal "123561636"))],
+   testCase "symbols string test ')%&!)(#' " $ parseString "')%&!)(#'" @?= Right [SExp (Const (StringVal ")%&!)(#"))],
+   testCase "non Ascii Symbols test '£¡½¡¥[±}{]]¥}¡½]}¡[½}'" $ case parseString "'£¡½¡¥[±}{]]¥}¡½]}¡[½}'" of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p,  
+   testCase "mix string test 'Niowe89359n9t!&&&#&&/()' " $ parseString "'Niowe89359n9t!&&&#&&/()'" @?= Right [SExp (Const (StringVal "Niowe89359n9t!&&&#&&/()"))],
+   testCase "strings with keywords test 'ifforinrangelist' " $ parseString "'ifforinrangelist'" @?= Right [SExp (Const (StringVal "ifforinrangelist"))],
+   testCase "Dirty string should fail" $ case parseString "wow!" of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p]
 -- testingFuncEval :: TestTree
 -- testingFuncEval = testGroup "TestingFuncEval"
