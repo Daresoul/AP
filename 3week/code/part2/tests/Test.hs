d29c62926aca8db299174ec19330177e89e62ffa
Mikkel <right-mik@hotmail.com>
2022-09-29 21:37:34 +0200

    tests

--- 3week/code/part2/tests/Test.hs
+++ 3week/code/part2/tests/Test.hs
@@ -31,6 +31,15 @@ testingMathOperators = testGroup "testingMathOperators"
    testCase "non Ascii Symbols test '£¡½¡¥[±}{]]¥}¡½]}¡[½}'" $ case parseString "'£¡½¡¥[±}{]]¥}¡½]}¡[½}'" of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p,  
    testCase "mix string test 'Niowe89359n9t!&&&#&&/()' " $ parseString "'Niowe89359n9t!&&&#&&/()'" @?= Right [SExp (Const (StringVal "Niowe89359n9t!&&&#&&/()"))],
    testCase "strings with keywords test 'ifforinrangelist' " $ parseString "'ifforinrangelist'" @?= Right [SExp (Const (StringVal "ifforinrangelist"))],
-   testCase "Dirty string should fail" $ case parseString "wow!" of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p]
--- testingFuncEval :: TestTree
--- testingFuncEval = testGroup "TestingFuncEval"
+   testCase "Dirty string symbol fail" $ case parseString "wow!" of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+   testCase "dirty addition symbol fail" $ case parseString "2!+2!"  of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+   testCase "dirty addition letter fail" $ case parseString "2awr+2awr!"  of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+   testCase "dirty addition symbol2 fail" $ case parseString "2...+2..0!"  of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+   testCase "dirty minus symbol fail" $ case parseString "2!-2!"  of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+   testCase "dirty minus letter fail" $ case parseString "2awr-2awr!"  of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+   testCase "dirty minus symbol2 fail" $ case parseString "2...-2..0!"  of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+   testCase "Times 2*2" $ parseString "2*2" @?= Right [SExp (Oper Times (Const (IntVal 2)) (Const (IntVal 2)))],
+   testCase "Times (-2)*2" $ parseString "(-2)*2" @?= Right [SExp (Oper Times (Const (IntVal (-2) )) (Const (IntVal 2)))],
+   testCase "Times -2*2" $ parseString "-2*2" @?= Right [SExp (Oper Times (Const (IntVal (-2) )) (Const (IntVal 2)))],
+   testCase "Times (-2)*(-2)" $ parseString "(-2)*(-2)" @?= Right [SExp (Oper Times (Const (IntVal (-2) )) (Const (IntVal (-2))))],
+   testCase "Times left asso 2*2*2" $ parseString "2*2*2" @?= Right [SExp (Oper Times (Oper Times (Const (IntVal 2)) (Const (IntVal 2))) (Const (IntVal 2)))]]
