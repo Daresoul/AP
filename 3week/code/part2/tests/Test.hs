9411e2e6b53f2c169dfc1cff8cea23c3f18672d5
Daresoul <nicolas.dyhrman@gmail.com>
2022-09-30 17:34:25 +0200

    added code

--- 3week/code/part2/tests/Test.hs
+++ 3week/code/part2/tests/Test.hs
@@ -102,5 +102,11 @@ testingMathOperators = testGroup "testingMathOperators"
    testCase "Greater Equal 3xfail 2>=2>=2 fail" $ case parseString "2>=2>=2"  of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p,
    testCase "Less 3xfail 2<2<2 fail" $ case parseString "2<2<2"  of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p,
    testCase "Greater 3xfail 2>2>2 fail" $ case parseString "2>2>2"  of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p,
-   testCase "Less Equal 3xfail 2<=2<=2 fail" $ case parseString "2<=2<=2"  of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p
+   testCase "Less Equal 3xfail 2<=2<=2 fail" $ case parseString "2<=2<=2"  of Left e -> return (); Right p -> assertFailure $ "Unexpected parse: " ++ show p,
+
+   testCase "List empty" $ parseString " [  ] " @?= Right [SExp (List [])],
+   testCase "List values" $ parseString " ['hej', 'med', 'dig', 1] " @?= Right [SExp (List [Const (StringVal "hej"), Const (StringVal "med"), Const (StringVal "dig"), Const (IntVal 1)])],
+   
+   testCase "List Comprehension" $ parseString "[x for y in 2+2 if 3]" @?= Right [SExp (Compr (Var "x") [CCFor "y" (Oper Plus (Const (IntVal 2)) (Const (IntVal 2))),CCIf (Const (IntVal 3))])],
+   testCase "List Comprehension advanced" $ parseString "[x for y in 2+2 if 3 if 4 for z in 5*5]" @?= Right [SExp (Compr (Var "x") [CCFor "y" (Oper Plus (Const (IntVal 2)) (Const (IntVal 2))),CCIf (Const (IntVal 3)),CCIf (Const (IntVal 4)),CCFor "z" (Oper Times (Const (IntVal 5)) (Const (IntVal 5)))])],
    ]
