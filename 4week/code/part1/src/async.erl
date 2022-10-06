04ea912e6dd3efd229aa4eec0a375bf10d666a55
Daresoul <nicolas.dyhrman@gmail.com>
2022-10-06 15:34:17 +0200

    working?

--- 4week/code/part1/src/async.erl
+++ 4week/code/part1/src/async.erl
@@ -2,7 +2,25 @@
 
 -export([new/2, wait/1, poll/1]).
 
-new(Fun, Arg) -> nope.
-wait(Aid) -> nope.
+-import(string, [len/1, concat/2]).
+
+new(Fun, Arg) ->
+  spawn(
+    fun() ->
+      Fun(Arg),
+      loop(val, beginning)
+    end).
+wait(Aid) -> request_reply(Aid, {value,done}).
 poll(Aid) -> nope.
 
+request_reply(Pid, Request) ->
+  Pid ! {self(), Request},
+  receive
+    {Pid, Response} -> Response
+  end.
+
+loop(Value, State) ->
+  receive
+    {From, {Fun, Arg}} ->
+      Value = Fun(Arg)
+  end.
